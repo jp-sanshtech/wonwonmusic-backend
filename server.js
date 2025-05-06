f2046c2abb2f7f522d02678e7aa10da0f66af6d3
jp-sanshtech <jp@sanshtech.ca>
2025-05-06 15:30:46 -0600

    first commit

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const session = require('express-session');
 const app = express();
 
